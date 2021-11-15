2557baadaba307332d323e3b14f07628a23def2e
Matt Rothenberg <mattrothenberg@github.com>
2021-11-15 13:53:06 -0500

    feat: update basic folder example

--- src/viewers/example-folder-viewer/index.tsx
+++ src/viewers/example-folder-viewer/index.tsx
@@ -4,15 +4,40 @@ export function Viewer(props: FolderViewerProps) {
   useTailwindCdn();
 
   return (
-    <div className="max-w-sm">
-      <p>This is the folder content.</p>
-      <ul className="divide-y">
-        {props.tree.map((item, index) => (
-          <li className="text-sm py-2" key={index}>
-            {item.path}
-          </li>
-        ))}
-      </ul>
+    <div className="p-8">
+      <p className="text-sm text-gray-600">This is the folder content.</p>
+      <div className="mt-2 borderborder-gray-200">
+        <table className="w-full divide-y divide-gray-200">
+          <thead className="bg-gray-50">
+            <tr>
+              <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                Path
+              </th>
+              <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                Size
+              </th>
+              <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                Type
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            {props.tree.map((item, index) => (
+              <tr className="even:bg-gray-50 odd:bg-white" key={index}>
+                <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
+                  {item.path}
+                </td>
+                <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
+                  {item.size}
+                </td>
+                <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
+                  {item.type}
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 }
