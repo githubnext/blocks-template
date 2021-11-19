92acc5a8017aec49b6a3dc3f17034ab4cb848357
Matt Rothenberg <mattrothenberg@github.com>
2021-11-19 10:55:11 -0500

    feat: build blocks to dist

---
+++ build.ts
@@ -0,0 +1,26 @@
+#!/usr/bin/env node
+const esbuild = require("esbuild");
+const path = require("path");
+const fs = require("fs");
+
+async function main() {
+  const pkg = JSON.parse(
+    fs.readFileSync(path.resolve(__dirname, "./package.json"), "utf8")
+  );
+
+  const blockBuildFuncs = pkg.blocks.map((block) => {
+    return esbuild.build({
+      entryPoints: [`./` + block.entry],
+      bundle: true,
+      outfile: `dist/${block.id}.js`,
+    });
+  });
+
+  try {
+    await Promise.all(blockBuildFuncs);
+  } catch (e) {
+    console.error("Error bundling blocks", e);
+  }
+}
+
+main();
