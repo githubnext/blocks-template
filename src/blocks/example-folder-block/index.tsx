7225b82a06ef5480825d17067ff29c0fe7a26386
Matt Rothenberg <mattrothenberg@github.com>
2022-02-25 11:39:43 -0500

    feat: use updated utils lib

--- src/blocks/example-folder-block/index.tsx
+++ src/blocks/example-folder-block/index.tsx
@@ -1,42 +1,26 @@
-import { FolderBlockProps, useTailwindCdn } from "@githubnext/utils";
+import { FolderBlockProps } from "@githubnext/utils";
 
 export default function (props: FolderBlockProps) {
-  useTailwindCdn();
-
   return (
     <div className="Box m-4">
       <div className="Box-header">
-        <h3 className="Box-title">
-          This is the folder content.
-        </h3>
+        <h3 className="Box-title">This is the folder content.</h3>
       </div>
       <div className="Box-body">
-        <table style={{ textAlign: "left", }}>
+        <table style={{ textAlign: "left" }}>
           <thead>
             <tr>
-              <th className="p-1">
-                Path
-              </th>
-              <th className="p-1">
-                Size
-              </th>
-              <th className="p-1">
-                Type
-              </th>
+              <th className="p-1">Path</th>
+              <th className="p-1">Size</th>
+              <th className="p-1">Type</th>
             </tr>
           </thead>
           <tbody>
             {props.tree.map((item, index) => (
               <tr key={index}>
-                <td className="p-1">
-                  {item.path}
-                </td>
-                <td className="p-1">
-                  {item.size}
-                </td>
-                <td className="p-1">
-                  {item.type}
-                </td>
+                <td className="p-1">{item.path}</td>
+                <td className="p-1">{item.size}</td>
+                <td className="p-1">{item.type}</td>
               </tr>
             ))}
           </tbody>
